3b1c2ec613b88acea6d079c96a0bca4ad61ff22c
jordonl27 <123746925+jordonl27@users.noreply.github.com>
2023-03-11 21:52:39 +0000

    netflix clone

---
+++ netflixclone/netflix.js
@@ -0,0 +1,41 @@
+const tabItems = document.querySelectorAll(`.tab-item`);
+const tabContentItems = document.querySelectorAll(`.tab-content-item`);
+//select tab content item
+function selectItem(e) {
+    //remove all show and border classes
+    removeBorder();
+    removeShow();
+    //add border to current tab item
+    this.classList.add(`tab-border`);
+    //grab content item from DOM
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    //add show class
+    tabContentItem.classList.add(`show`);     }
+
+
+//remove bottom borders from all tab items 
+function removeBorder() {
+    tabItems.forEach(item => {
+        item.classLIist.remove(`tab-border`);
+
+    });
+}
+
+//remove show class from all content items
+function removeShow() {
+    tabContentItems.forEach(item => {
+        item.classList.remove(`show`);
+    });
+}
+
+// listen for tab item click
+tabItems.forEach(item => {
+    item.addEventListener(`click`, selectItem);
+});
+
+
+
+
+
+
+
